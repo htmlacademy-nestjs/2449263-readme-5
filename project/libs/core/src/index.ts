acd47a75ccfbc3aa378cb62e5f7e925804c592d3
asemashkin <asemashkin@adv-ms.com>
2024-01-31 13:58:10 +0300

    Добавляет валидацию загружаемого файла

--- project/libs/core/src/index.ts
+++ project/libs/core/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/repository/base-memory.repository';
 export * from './lib/repository/base-mongo.repository';
 export * from './lib/repository/base-postgres.repository';
 export * from './lib/pipes/mongo-id-validation.pipe';
+export * from './lib/pipes/file-picture-validation.pipe';
